4d1c7b837dd52bd6afa23ce4ef05aadc83ed84d7
SzamanHerbaty <kuba.pietrasik1234@gmail.com>
2026-07-18 19:42:51 +0200

    Co zrobiono: Dodanie wpisywania liczby, kropki i działania operatorów Do poprawy: Działania na ułamkach

--- script.js
+++ script.js
@@ -1,19 +1,138 @@
 const operators = document.querySelectorAll(".operator");
 const number = document.querySelectorAll(".number");
-const equals = document.querySelector(".equals");
-const negate = document.querySelector(".negate");
-const del = document.querySelector(".delete");
-const clear = document.querySelector(".clear");
+const equalsBtn = document.querySelector(".equals");
+const negateBtn = document.querySelector(".negate");
+const delBtn = document.querySelector(".delete");
+const clearBtn = document.querySelector(".clear");
+const display = document.querySelector(".input");
+const dot = document.querySelector(".dot");
 
-function appendClick(item){
-    item.addEventListener("click", () => {
-        console.log(item.textContent);
-    })
+console.log(dot.textContent)
+
+let state = {
+    firstNumber: "",
+    secondNumber: "",
+    isDotHere: false,
+    clearNeed: false,
+    currentOperator: ""
+};
+
+// MATH FUNCTIONS
+function operate(op, a, b){
+  switch (op) {
+    case "+":
+      return a + b;
+      break;
+    case "-":
+      return a - b;
+      break;
+    case "*":
+      return a * b;
+      break;
+    case "/":
+      if (b === 0) {
+        return "! ERROR !";
+      } else {
+        return a / b;
+      }
+  }
 }
 
-operators.forEach(appendClick);
-number.forEach(appendClick);
-appendClick(equals);
-appendClick(negate);
-appendClick(del);
-appendClick(clear);
+function removeZeros(str){
+    return str.replace(/^0+(?!$)/, '');
+}
+
+
+// CALCULATOR DIPSLAY FUNCTIONS
+function addToDipslay(value){
+    display.textContent = value;
+}
+
+
+function numberInput(item){
+
+    
+    if(state.clearNeed){
+        display.textContent = "";
+        state.clearNeed = false;
+    }
+
+    if(state.currentOperator === ""){
+        state.firstNumber += item.textContent;
+       // state.firstNumber = removeZeros(state.firstNumber);
+        console.log(state);
+        addToDipslay(state.firstNumber);
+    }
+    else{
+        state.secondNumber += item.textContent;
+       // state.secondNumber = removeZeros(state.secondNumber);
+        addToDipslay(state.secondNumber);
+    }
+
+}
+
+
+function operatorInput(item){
+
+    console.log(state);
+
+    if(state.currentOperator !== "" && state.secondNumber === ""){
+        state.currentOperator = item.textContent;
+    }
+    
+    if(state.currentOperator !== "" && state.secondNumber !== ""){
+        state.firstNumber = operate(state.currentOperator, parseFloat(state.firstNumber), parseFloat(state.secondNumber))
+        state.secondNumber = "";
+        addToDipslay(state.firstNumber);
+    }
+
+    state.currentOperator = item.textContent;
+    state.clearNeed = true;
+}
+
+function dotInput(item){ 
+    console.log(state);
+
+    if(state.clearNeed){
+        display.textContent = "";
+        state.clearNeed = false;
+    }
+
+
+      if (state.currentOperator === "") {
+        
+        if (state.firstNumber === "") state.firstNumber = "0.";
+        else if (!state.firstNumber.includes(".")) state.firstNumber += ".";
+        addToDipslay(state.firstNumber);
+      }
+      else{
+        if (state.secondNumber === "") state.secondNumber = "0.";
+        else if (!state.secondNumber.includes(".")) state.secondNumber += ".";
+        addToDipslay(state.secondNumber);
+      }
+      
+    
+}
+
+
+// NUMBER EVENTS
+
+number.forEach((item) => {
+    item.addEventListener("click", () =>{
+        numberInput(item);
+    });
+});
+
+
+// OPERATOR EVENTS
+
+operators.forEach((item) => {
+    item.addEventListener("click", () => {
+        operatorInput(item);
+    })
+})
+
+dot.addEventListener("click", () => {
+    dotInput(dot);
+})
+
