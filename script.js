d3a0bdacaf186ff2bb7f443baa60d3d7737362bd
SzamanHerbaty <kuba.pietrasik1234@gmail.com>
2026-07-20 02:12:01 +0200

    dodanie podświetlenia operatorów oraz funkcjonalności przycisku AC

--- script.js
+++ script.js
@@ -9,13 +9,38 @@ const dot = document.querySelector(".dot");
 
 console.log(dot.textContent)
 
+/*STATE OBJECT - jest odpowiedzialny za przetrzymywanie zmienny informujących o stanie w jakim jest kalkulator
+                 Wartości tego obiektu zmieniają się wraz z różnymi opracjami
+
+    firstNumber - pierwsza liczba potrzebna do działania 
+    secondNumber - druga liczba potrzebna do działania 
+    clearNeed - czy po naciśnięciu jakiejś liczby jest potrzebne wyczyszczenie wyświetlacza kalkulatora
+    currentOperator - obecny operator
+    
+*/
 let state = {
     firstNumber: "",
     secondNumber: "",
-    isDotHere: false,
     clearNeed: false,
     currentOperator: ""
 };
+
+function resetState(){
+
+    clearHighlight();
+
+    state.firstNumber = "";
+    state.secondNumber = "";
+    state.clearNeed = false;
+    state.currentOperator = "";
+
+}
+
+function errorCheck(){
+    if(state.firstNumber === "! ERROR !"){
+        resetState();
+    }
+}
 
 // MATH FUNCTIONS
 function operate(op, a, b){
@@ -48,22 +73,26 @@ function addToDipslay(value){
     display.textContent = value;
 }
 
-
+// NUMBER INPUT FUNCTION
 function numberInput(item){
 
-    
+
     if(state.clearNeed){
         display.textContent = "";
         state.clearNeed = false;
     }
 
+    errorCheck();
+
     if(state.currentOperator === ""){
         state.firstNumber += item.textContent;
        // state.firstNumber = removeZeros(state.firstNumber);
-        console.log(state);
         addToDipslay(state.firstNumber);
     }
     else{
+        if (state.firstNumber === ""){
+            state.firstNumber = "0";
+        }
         state.secondNumber += item.textContent;
        // state.secondNumber = removeZeros(state.secondNumber);
         addToDipslay(state.secondNumber);
@@ -71,10 +100,10 @@ function numberInput(item){
 
 }
 
-
+// OPERATOR INPUT FUNCTION
 function operatorInput(item){
 
-    console.log(state);
+    errorCheck();
 
     if(state.currentOperator !== "" && state.secondNumber === ""){
         state.currentOperator = item.textContent;
@@ -90,6 +119,8 @@ function operatorInput(item){
     state.clearNeed = true;
 }
 
+// DOT INPUT
+
 function dotInput(item){ 
     console.log(state);
 
@@ -97,22 +128,62 @@ function dotInput(item){
         display.textContent = "";
         state.clearNeed = false;
     }
-
-
-      if (state.currentOperator === "") {
+    if (state.currentOperator === "") {
         
         if (state.firstNumber === "") state.firstNumber = "0.";
         else if (!state.firstNumber.includes(".")) state.firstNumber += ".";
         addToDipslay(state.firstNumber);
-      }
-      else{
+    }
+    else{
         if (state.secondNumber === "") state.secondNumber = "0.";
         else if (!state.secondNumber.includes(".")) state.secondNumber += ".";
         addToDipslay(state.secondNumber);
-      }
+    }
       
     
 }
+
+// EQUALS FUNCTION
+
+function equalsInput(){
+
+    errorCheck();
+
+    if(state.firstNumber !== "" && state.secondNumber !== ""){
+        state.firstNumber = operate(state.currentOperator, parseFloat(state.firstNumber), parseFloat(state.secondNumber))
+        state.secondNumber = "";
+        state.currentOperator = "";
+        addToDipslay(state.firstNumber);
+        clearHighlight();
+    }
+
+    console.log(state);
+
+}
+
+// CURRENT OPERATOR HIGHLIGHT 
+
+function clearHighlight(){
+    operators.forEach((item) => {
+        item.classList.remove("highlight");
+    })
+}
+
+function operatorHighlight(op){
+
+    clearHighlight();
+
+    op.classList.add("highlight");
+
+}
+
+// CLEAR BUTTON FUNCTION - resetuje całkowicie obiekt state i wyświetlacz kalkulatora
+
+function clearAll(){
+    display.textContent = "0";
+    resetState();
+}
+
 
 
 // NUMBER EVENTS
@@ -129,10 +200,20 @@ number.forEach((item) => {
 operators.forEach((item) => {
     item.addEventListener("click", () => {
         operatorInput(item);
+        operatorHighlight(item);
     })
 })
+
+// DOT EVENT
 
 dot.addEventListener("click", () => {
     dotInput(dot);
 })
 
+// CLEAR EVENT
+
+clearBtn.addEventListener("click", clearAll);
+
+// EQUALS 
+
+equalsBtn.addEventListener("click", equalsInput);
